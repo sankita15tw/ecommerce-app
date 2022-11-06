721e7b14ff5472a225f3b1591e978d6188a8c88a
Ankita Srivastava <sankita@thoughtworks.com>
2022-11-06 13:17:48 +0100

    Add products and categories

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-import { ApolloServer, gql } from "apollo-server"
+import {ApolloServer, gql} from "apollo-server"
+import { products, categories } from "./db.js"
 
-const typeDefs = gql `
+const typeDefs = gql`
+    type Product {
+        id: ID!
+        name: String
+        description: String
+        quantity: Int
+        price: Float
+        image: String
+        onSale: Boolean
+    }
+    
+    type Category {
+        id: ID!
+        name: String
+    }
+
     type Query {
         hello: String
         age: Int
+        product(id: String!) : Product
+        products: [Product!]!
+        categories: [Category!]!
     }
 `
 
 const resolvers = {
     Query: {
         hello: () => "Hello GraphQL",
-        age: () => 26
+        age: () => 26,
+        products: () => {
+            return products
+        },
+        categories: () => {
+            return categories
+        },
+        product: (parent, { id }, context) => {
+            return products.find(product => product.id === id)
+        }
     }
 }
 
