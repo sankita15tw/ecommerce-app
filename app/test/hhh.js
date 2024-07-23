a998f7671466c6deab2decbb6b5937e87dd22474
Ankita Srivastava <sankita@thoughtworks.com>
2024-07-23 17:43:43 +0530

    commit to be reverted 10

---
+++ app/test/hhh.js
@@ -0,0 +1,5 @@
+const obj = {
+    dirty: 'code'
+}
+
+export default obj
--- app/test/sample.js
+++ app/test/sample.js
@@ -1,1 +1,3 @@
-console.log('Testing workflows123 djkb');
+import obj from "./hhh";
+
+console.log('Testing workflows123 djkb hsdbhjvbdfhjvbdfjhbvdfhjbvdf dfhvbhjdfbvd dbfhudbuv isdfvbuev sfdbvuirbv sbdfuvibder siudfvhgiefurgbv sdfiuvgebuygberfberufbreuibfurbe dvbefwurygfeuyr');
